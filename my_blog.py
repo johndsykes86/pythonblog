7eda075465e1efa8866154410b19ca75a0d8bd68
Johnathon Sykes <johndsykes86@gmail.com>
2017-02-09 20:57:02 -0800

    added static pages for blog

--- my_blog.py
+++ my_blog.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 #setting up URL!
 @app.route("/")
 
-def somename ():
-    return "Hello world!"
+def index ():
+    return app.send_static_file('index.html')
+
+@app.route('/about')
+def about():
+    return app.send_static_file('about.html')
+
+@app.route('/contact')
+def contact():
+    return app.send_static_file('contact.html')
